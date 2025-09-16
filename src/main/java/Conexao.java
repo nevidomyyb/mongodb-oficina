4d328c66dd73b5194b727f5290fdfe2b05724c4c
Pedro Cunha <idk.pedroc@gmail.com>
2025-09-16 16:20:46 -0300

    add: conection class

---
+++ src/main/java/Conexao.java
@@ -0,0 +1,20 @@
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+public class Conexao {
+
+    private MongoClient cliente;
+    private MongoDatabase database;
+
+    public Conexao() {
+        this.cliente = MongoClients.create("mongodb://root:Senhals@localhost:27017");
+        this.database = this.cliente.getDatabase("oficina");
+    }
+
+    public MongoDatabase getBanco() {return database;}
+
+    public static void main(String[] args) {
+        Conexao c = new Conexao();
+    }
+}
